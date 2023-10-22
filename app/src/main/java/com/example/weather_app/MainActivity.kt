fd0fefbe85dee35d771b104f5a7198131eb108ec
UmanGeZ <umangaraval21@gnu.ac.in>
2023-10-22 13:12:04 +0530

    managing Api From Open Weather

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.weather_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//api key e37b98e3a86353309288d5ea7497c99e
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
